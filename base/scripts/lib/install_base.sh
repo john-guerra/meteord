76db7df8ade29929b48e4d0996e0f98368aaee6f
John Alexis Guerra Gómez <john.guerra@gmail.com>
2017-05-27 11:32:51 -0500

    Adding imagemagick scripts

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git
+apt-get install -y curl bzip2 build-essential g++ python git libmagick++-dev imagemagick
+echo "export PATH=/usr/lib/x86_64-linux-gnu/ImageMagick-6.8.9/bin-Q16:$PATH" >> /etc/profile
+ln -s /usr/lib/x86_64-linux-gnu/ImageMagick-6.8.9/bin-Q16/Magick++* /usr/bin
+source ~/.bashrc
